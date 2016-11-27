9f9d8d3cb3ec83b9006d0ee78b5a31f855b18eef
Vistor25 <victorvoronko@gmail.com>
2016-11-28 01:36:49 +0300

    add

--- Task2/BinarySearchTree.cs
+++ Task2/BinarySearchTree.cs
@@ -1,25 +1,115 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Task2
 {
-    public class BinarySearchTree<T>
+    public class BinarySearchTree<T>:IEnumerable<T>
     {
+        private Node<T> root;
+        private IComparer<T> comparer;
+
         class Node<T>
         {
-            private T Value { get; set; }
-            private Node<T> leftNode;
-            private Node<T> rightNode;
+            public T Value { get; set; }
+            public Node<T> leftNode;
+            public Node<T> rightNode;
+            public Node(T element)
+            {
+                Value = element;
+            }
 
         }
 
         public void Add(T element)
         {
-            var current;
-            while
+            Node<T> current = root;
+            while (!ReferenceEquals(current, null))
+            {
+                if (comparer.Compare(current.Value, element) > 0)
+                {
+                    current = current.rightNode;}
+                if(comparer.Compare(current.Value, element) == 0) { throw new ArgumentException("There is already such element!");}
+                if (comparer.Compare(current.Value, element) < 0)
+                {
+                    current = current.leftNode;
+                }
+            }
+            current = new Node<T>(element); 
+        }
+
+        public bool Contains(T element)
+        {
+            Node<T> current = root;
+            while (!ReferenceEquals(current, null))
+            {
+                if (comparer.Compare(current.Value, element) > 0)
+                {
+                    current = current.rightNode;
+                }
+                if (comparer.Compare(current.Value, element) == 0) {return true; }
+                if (comparer.Compare(current.Value, element) < 0)
+                {
+                    current = current.leftNode;
+                }
+            }
+            return false;
+        }
+
+        private IEnumerable<T> PreOrder(Node<T> node)
+        {
+            if(ReferenceEquals(node, null)) { yield break;}
+            yield return node.Value;
+            foreach (var element in PreOrder(node.leftNode))
+            {
+                yield return element;
+            }
+            foreach (var element in PreOrder(node.rightNode))
+            {
+                yield return element;
+            }
+            
+        }
+
+        private IEnumerable<T> InOrder(Node<T> node)
+        {
+            if (ReferenceEquals(node, null)) { yield break; }
+            foreach (var element in InOrder(node.leftNode))
+            {
+                yield return element;
+            }
+            yield return node.Value;
+            foreach (var element in InOrder(node.rightNode))
+            {
+                yield return element;
+            }
+        }
+
+        private IEnumerable<T> PostOrder(Node<T> node)
+        {
+            if (ReferenceEquals(node, null)) { yield break; }
+            foreach (var element in PostOrder(node.leftNode))
+            {
+                yield return element;
+            }
+            foreach (var element in PostOrder(node.rightNode))
+            {
+                yield return element;
+            }
+            yield return node.Value;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return PreOrder(root).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+           return GetEnumerator();
         }
     }
 }
