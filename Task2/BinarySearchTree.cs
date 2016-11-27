26b6a3f6548fd9c8c93dfeb6c4b3cd9a137b7c8f
Vistor25 <victorvoronko@gmail.com>
2016-11-27 20:28:12 +0300

    add

---
+++ Task2/BinarySearchTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class BinarySearchTree<T>
+    {
+        class Node<T>
+        {
+            private T Value { get; set; }
+            private Node<T> leftNode;
+            private Node<T> rightNode;
+
+        }
+
+        public void Add(T element)
+        {
+            var current;
+            while
+        }
+    }
+}
