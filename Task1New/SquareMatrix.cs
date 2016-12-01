86fe5520f0017b7561044ce6630aa30def10943b
Vistor25 <victorvoronko@gmail.com>
2016-12-01 19:33:00 +0300

    add

--- Task1New/SquareMatrix.cs
+++ Task1New/SquareMatrix.cs
@@ -8,9 +8,9 @@ namespace Task1New
 {
     public class SquareMatrix<T>:AbstarctMatrix<T> where T: struct
     {
-        private readonly T[,] _data;
+        private readonly T[] _data;
 
-        public int Size => _data.GetLength(0);
+        public int Size;
 
         public SquareMatrix(T[,] source)
         {
@@ -18,42 +18,27 @@ namespace Task1New
             {
                 throw new ArgumentException("Can't create the matrix from provided array.");
             }
-
-            _data = source;
-        }
-
-        public event Action<T, T, int, int> ElementChanged;
-
-        public T this[int i, int j]
-        {
-            get
+            Size = source.GetLength(0);
+            _data = new T[Size*Size];
+            int count=0;
+            for(int i= 0; i<Size; i++)
             {
-                return _data[i, j];
+                for(int j = 0; j < Size; j++)
+                {
+                    _data[count++] = source[i, j];
+                }
             }
-            set
-            {
-                var previousValue = _data[i, j];
-                _data[i, j] = value;
-                OnElementChanged(previousValue, value, i, j);
-            }
-        }
-
-        protected virtual void OnElementChanged(T arg1, T arg2, int arg3,
-            int arg4)
-        {
-            ElementChanged?.Invoke(arg1, arg2, arg3, arg4);
         }
 
        
-
         public void Accept(IVisitor<T> visitor, SquareMatrix<T> other)
         {
             visitor.Visit((dynamic) this, other);
         }
 
         protected override T GetElement(int i, int j)
-        {
-            throw new NotImplementedException();
+        {            
+            return _data[i*Size]
         }
 
         protected override void SetElement(T element, int i, int j)
